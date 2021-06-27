0cd8451ccb07d9a8a2e6d5a5e2ded99ee8147196
nadakomo <76624003+nadakomo@users.noreply.github.com>
2021-06-28 00:13:15 +0300

    Update SM_90_Code.ino

--- SM_90_Code.ino
+++ SM_90_Code.ino
@@ -1,13 +1,27 @@
 #include <Servo.h>
 
-int ServoAng=90;
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+Servo Servo4;
+Servo Servo5;
 
-Servo myServo;
+int servoAngle=90;
 
 void setup() {
-myServo.attach(9);
+  
+  Servo1.attach(5);
+  Servo2.attach(6);
+  Servo3.attach(7);
+  Servo4.attach(8);
+  Servo5.attach(9);
 }
 
 void loop() {
-myServo.write(ServoAng);
+  
+  Servo1.write(servoAngle);
+  Servo2.write(servoAngle);
+  Servo3.write(servoAngle);
+  Servo4.write(servoAngle);
+  Servo5.write(servoAngle);
 }
